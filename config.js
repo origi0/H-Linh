e4987568eef6bc0444da725c7c8588573ffae425
origi0 <157888519+origi0@users.noreply.github.com>
2024-04-01 17:26:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Gửi Hà Linh 😖",
     introTitle: 'Phạm Hà Linh',
-    introDesc: `Google thì có đường link, còn Cường thì chỉ có một mình Linh thôi.`,
+    introDesc: `Google thì có đường link, còn mik thì chỉ có một mình Linh thôi.`,
     btnIntro: '^^UwU^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thik mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Cá tháng tư vui vẻ, trôn trôn việt nam =))).',
+    messDesc: 'Cá tháng tư vui vẻ, trôn trôn việt nam 🤡🤡🤡.',
     btnAccept: ':<3',
     messLink: 'https://www.youtube.com/watch?v=eyKbnfQYnYg' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
