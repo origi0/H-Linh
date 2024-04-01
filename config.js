150c728a8e1b49c6b6ff2a3239a3986589c59652
origi0 <157888519+origi0@users.noreply.github.com>
2024-04-01 12:45:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Gửi Hà Linh 😖",
+    introTitle: 'Phạm Hà Linh',
+    introDesc: `Google thì có đường link, còn Cường thì chỉ có một mình Linh thôi.`,
+    btnIntro: '^^UwU^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
@@ -14,7 +11,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: 'Cá tháng tư vui vẻ, trôn trôn việt nam =))).',
+    btnAccept: ':<3',
+    messLink: 'https://www.youtube.com/watch?v=eyKbnfQYnYg' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
